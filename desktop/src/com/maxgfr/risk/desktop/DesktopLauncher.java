254aa8462504a969d499afbabe4c550fc569eadb
Maxime Golfier <maxgolfier@gmail.com>
2018-04-30 13:45:20 +0200

    message

--- desktop/src/com/maxgfr/risk/desktop/DesktopLauncher.java
+++ desktop/src/com/maxgfr/risk/desktop/DesktopLauncher.java
@@ -8,5 +8,6 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new Risk(), config);
+		System.out.println("lol");
 	}
 }
