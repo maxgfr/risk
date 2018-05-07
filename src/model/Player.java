67d3ecd9efd70eb26e639b356e47fe0007c36b50
Maxime Golfier <maxgolfier@gmail.com>
2018-05-07 11:42:58 +0200

    message

---
+++ src/model/Player.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author golfier
+ */
+public class Player {
+	
+	private String name;
+	private int id;
+	private List<Territory> mTerritories;
+	private List<Card> cards;
+	private int nbSold;
+	
+	Player(String name, int id) {
+		this.name = name;
+		this.id = id;
+		mTerritories = new ArrayList<Territory>();
+                cards = new ArrayList<Card>();
+                nbSold = 0;
+	}
+	
+	public int getId(){
+		return id;
+	}
+	
+	public String getName(){
+		return name;
+	}
+
+	public List<Territory> getOccupiedTerritories(){
+		return mTerritories;
+	}
+
+	public int numOfTerritories(){
+		return mTerritories.size();
+	}
+	
+	public void occupyTerritory(Territory t){
+		mTerritories.add(t);
+	}
+	
+	public void looseTerritory(Territory t){
+		mTerritories.remove(t);
+	}
+
+        public void setCard(Card c){
+            cards.add(c);
+        }
+
+        public List<Card> getCard(){
+            return cards;
+        }
+	
+	public void addArmies(int a){
+		nbSold += a;
+	}
+
+        public void addArmy(){
+            nbSold++;
+        }
+
+        public void looseArmies(int a){
+            nbSold -= a;
+        }
+        
+        public void looseArmy(){
+            nbSold--;
+        }
+	
+	
+}
+
--- src/risk/Risk.java
+++ src/risk/Risk.java
@@ -1,9 +1,11 @@
+package risk;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package risk;
+
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -19,7 +21,7 @@ public class Risk extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
         
