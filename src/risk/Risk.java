af44eab5b929778a74e77fbeb7df7f4f81c55dba
Maxime Golfier <maxgolfier@gmail.com>
2018-05-11 17:48:05 +0200

    message

--- src/risk/Risk.java
+++ src/risk/Risk.java
@@ -7,11 +7,15 @@ package risk;
  */
 
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javax.imageio.ImageIO;
 
 /**
  *
@@ -27,6 +31,8 @@ public class Risk extends Application {
         
         stage.setScene(scene);
         stage.show();
+        
+        iteratePixels();
     }
 
     /**
@@ -34,6 +40,39 @@ public class Risk extends Application {
      */
     public static void main(String[] args) {
         launch(args);
+    }
+    
+    
+    public static void iteratePixels() throws IOException {
+        File file = new File("/assets/image_fixed.png");
+        BufferedImage image = ImageIO.read(file);
+        int lastNotBlack = -1;
+        int actualColour = 0;
+        
+        System.out.println("KKK");
+
+        for (int x = 0; x < image.getHeight(); x++) {
+            for (int y = 0; y < image.getWidth(); y++) {
+
+                int black = -16777216;
+
+                try {
+                    actualColour = image.getRGB(x, y);
+
+                } catch (Exception e) {
+                    continue;
+                }
+                if(image.getRGB(x, y)==black){
+                    image.setRGB(x, y, lastNotBlack);
+
+                    System.out.println("black pixel at: " +x +" "+y);
+                }
+                if (actualColour != black){
+                    lastNotBlack= actualColour;
+                }
+
+            }
+        }
     }
     
 }
